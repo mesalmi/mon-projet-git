7ebcfc9e7099e36bcc2b93b17671baeabfbfb992
mesalmi <msalmizied@gmail.com>
2017-02-27 11:41:27 +0100

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name monProjetGitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the monProjetGitApp
+ */
+angular.module('monProjetGitApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
